5c6943121d334d55f20c45a780d3bbf81fa7c7c6
chenlong <woodccc@foxmail.com>
2019-08-08 20:07:45 +0800

    分页加载电影列表数据

---
+++ lib/api/model/movie.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Movie _$MovieFromJson(Map<String, dynamic> json) {
+  return Movie(
+    json['title'] as String,
+  );
+}
+
+Map<String, dynamic> _$MovieToJson(Movie instance) => <String, dynamic>{
+      'title': instance.title,
+    };
--- lib/pages/page_one.dart
+++ lib/pages/page_one.dart
@@ -6,6 +6,8 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'package:github_demo/api/api.dart' as api;
 
+import 'package:github_demo/api/model/movie.dart';
+
 class PageOne extends StatefulWidget {
   @override
   _PageOneState createState() => _PageOneState();
@@ -18,8 +20,9 @@ class _PageOneState extends State<PageOne> with AutomaticKeepAliveClientMixin {
   final ScrollController _scrollController = new ScrollController();
 
   static const loadingTag = "##loading##";
-  var _words = <String>[loadingTag];
+  var _words = <dynamic>[loadingTag];
   var _noMore = false;
+  var _total = 0;
 
   @override
   void initState() {
@@ -32,41 +35,18 @@ class _PageOneState extends State<PageOne> with AutomaticKeepAliveClientMixin {
         this._loadData();
       }
     });
-    api.getMovieList()
-      .then((data) {
-        print('------------------------');
-        print(data.title);
-      });
   }
 
   _loadData() {
     if (_noMore) return;
 
-    Future.delayed(Duration(seconds: 2)).then((e) {
-      var _newWords = [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10"
-      ].toList();
-      _words.insertAll(_words.length - 1, _newWords);
-      if (_words.length > 100) {
+    api.getMovieList(start: _words.length, count: 13).then((data) {
+      var _subjects = data.subjects.toList();
+
+      _words.insertAll(_words.length - 1, _subjects);
+      _total = data.total;
+
+      if (_words.length >= _total) {
         _noMore = true;
       }
       setState(() {});
@@ -94,10 +74,11 @@ class _PageOneState extends State<PageOne> with AutomaticKeepAliveClientMixin {
       }
     }
 
+    var movie = Movie.fromJson(_words[index]);
+
     return ListTile(
-      title: Text(_words[index]),
-      trailing: Icon(IconData(0xe600, fontFamily: "iconfont"), size: 16.0)
-    );
+        title: Text(movie.title),
+        trailing: Icon(IconData(0xe600, fontFamily: "iconfont"), size: 16.0));
   }
 
   @override
